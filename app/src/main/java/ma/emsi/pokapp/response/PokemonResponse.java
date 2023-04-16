f670220e588cee8b12f7f7f363d52525c82d1576
ismai <ismaini.hamid@gmail.com>
2023-04-16 03:57:19 +0000

    Pokémon API

--- app/src/main/java/ma/emsi/pokapp/response/PokemonResponse.java
+++ app/src/main/java/ma/emsi/pokapp/response/PokemonResponse.java
@@ -5,14 +5,16 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ma.emsi.pokapp.models.Pokemon;
 
+@NoArgsConstructor
+@Getter(value = AccessLevel.PUBLIC)
+@Setter(value = AccessLevel.PUBLIC)
 public class PokemonResponse {
     @SerializedName("results")
     private List<Pokemon> pokemonCall = new ArrayList<>();
-
-    public PokemonResponse() {  }
-
-    public List<Pokemon> getPokemonCall() { return pokemonCall; }
-    public void setPokemonCall(List<Pokemon> pokemonCall) { this.pokemonCall = pokemonCall; }
 }
